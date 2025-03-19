54ad410afdaded486a52d679359fcb5655cbd106
samani21 <eksloba21@gamil.com>
2025-03-20 04:16:14 +0800

    Mengganti link yt

--- src/pages/LandingPage/Hero.js
+++ src/pages/LandingPage/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => {
                 </TextSection>
                 <VideSection>
                     <VideoWrapper>
-                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/UieRlrc_FnE?si=zpysp7YP7qJPthWj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/7A-hUcBe2d8?si=enJAUkAJevrjY7Im" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                     </VideoWrapper>
                 </VideSection>
             </Content>
