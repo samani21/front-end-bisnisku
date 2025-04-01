d5a17655e96fcee0140853e0537adab3b8c316a0
samani21 <eksloba21@gamil.com>
2025-04-01 14:21:55 +0800

    Perbaiki tiktok pixel

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,38 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-        
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                !function (w, d, t) {
+                  w.TiktokAnalyticsObject=t;
+                  var ttq=w[t]=w[t]||[];
+                  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+                  ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}; 
+                  for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]); 
+                  ttq.instance=function(t){for(
+                    var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]); 
+                    return e
+                  },ttq.load=function(e,n){
+                    var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
+                    ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};
+                    n=document.createElement("script");
+                    n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;
+                    e=document.getElementsByTagName("script")[0];
+                    e.parentNode.insertBefore(n,e)
+                  };
+                  ttq.load('CVLO8T3C77U02UO7U3L0');
+                  ttq.page();
+                  ttq.identify({
+          email: 'user@example.com', 
+          phone_number: '+62123456789', 
+          external_id: '1234634'
+        });
+
+                }(window, document, 'ttq');
+              `,
+          }}
+        />
       </Head>
       <LandingPage />
     </>
