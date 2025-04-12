19427e8568dcf754c0e682a5d916fee688bedca8
samani21 <eksloba21@gamil.com>
2025-04-12 20:27:02 +0800

    Menambah tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./app/**/*.{js,ts,jsx,tsx}", // kalau kamu pakai folder app
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
